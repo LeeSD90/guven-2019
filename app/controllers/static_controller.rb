851e3f729eb68f00a4b3921a462783e3eb987078
LeeSD90 <lee.doyle90@gmail.com>
2019-05-11 10:14:56 +0200

    Add Static page routing and controller action

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,12 @@
 class StaticController < ApplicationController
   def index
   end
+  
+  def show
+    if template_exists?("#{params[:static_page]}", _prefixes)
+      render template: "static/#{params[:static_page]}"
+    else
+      render file: "#{Rails.root}/public/404", status: :not_found
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root :to => "static#index"
-
+  get "/:static_page" => "static#show", :as => "static_page"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
